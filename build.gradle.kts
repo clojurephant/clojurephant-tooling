cb672a190f4cbccdc1282e1ef948053d56744097
Andrew Oberstar <andrew.oberstar@outlook.com>
2022-08-07 20:44:16 -0500

    patch(build): Properly expose dependencies
    The tooling API is now bundled into the JAR. Other dependencies are
    exposed via feature variants. Consumers using figwheel should depend
    on those specifically. Though in many cases, they might have direct
    dependencies avoiding the need to use the feature variants.
    
    We likely won't be able to publish this to Clojars, due to their
    current lack of support for Gradle's module metadata. However, we
    should be able to publish to Central while waiting for any solution
    from Clojars themselves.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,35 +20,53 @@ java {
     languageVersion.set(JavaLanguageVersion.of(8))
   }
   withSourcesJar()
+
+  registerFeature("figwheelRepl") {
+    usingSourceSet(sourceSets["main"])
+  }
+  registerFeature("figwheelMain") {
+    usingSourceSet(sourceSets["main"])
+  }
 }
 
 dependencies {
-  implementation("org.clojure:clojure:1.11.1")
-  implementation("org.gradle:gradle-tooling-api:7.5")
+  // clojure
+  api("org.clojure:clojure:1.11.1")
+
+  // gradle
+  compileOnlyApi("org.gradle:gradle-tooling-api:7.5")
+
+  // clojurescript and nrepl
+  api("org.clojure:clojurescript:1.11.60")
+  api("nrepl:nrepl:0.9.0")
+  api("cider:piggieback:0.5.3")
+
+  // figwheel repl
+  "figwheelReplApi"("com.bhauman:figwheel-repl:0.2.18")
+  "figwheelReplApi"("ring:ring-jetty-adapter:1.9.5")
+  "figwheelReplApi"("org.eclipse.jetty.websocket:websocket-server:9.4.7.v20180619")
+
+  // figwheel main
+  "figwheelMainApi"("com.bhauman:figwheel-main:0.2.18")
+
+  // testing
   testRuntimeOnly("org.ajoberstar:jovial:0.3.0")
   devRuntimeOnly("org.slf4j:slf4j-simple:1.7.36")
-
-  compileOnly("org.clojure:clojurescript:1.11.60")
-  devImplementation("org.clojure:clojurescript:1.11.60")
-
-  compileOnly("nrepl:nrepl:0.9.0")
-  compileOnly("cider:piggieback:0.5.3")
-  devImplementation("cider:piggieback:0.5.3")
-
-  compileOnly("com.bhauman:figwheel-repl:0.2.18")
-  devImplementation("com.bhauman:figwheel-repl:0.2.18")
-
-  compileOnly("ring:ring-jetty-adapter:1.9.5")
-  devImplementation("ring:ring-jetty-adapter:1.9.5")
-  compileOnly("org.eclipse.jetty.websocket:websocket-server:9.4.7.v20180619")
-  devImplementation("org.eclipse.jetty.websocket:websocket-server:9.4.7.v20180619")
-
-  compileOnly("com.bhauman:figwheel-main:0.2.18")
-  devImplementation("com.bhauman:figwheel-main:0.2.18")
 }
 
 tasks.withType<Test>() {
   useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+  dependsOn(configurations.compileClasspath)
+  from({
+    configurations.compileClasspath.files { dep ->
+      dep.getGroup() == "org.gradle"
+    }.map {
+      zipTree(it).matching { include("org/gradle/**/*") }
+    }
+  })
 }
 
 publishing {
@@ -68,8 +86,4 @@ publishing {
       from(components["java"])
     }
   }
-}
-
-tasks.withType<GenerateModuleMetadata>() {
-  enabled = false
 }
